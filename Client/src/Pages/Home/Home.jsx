38a06ddfa85b33a8e3d2e3db90d128379c7512f6
Fahil haneef <Fahilhaneef006@gmail.com>
2023-11-22 01:05:55 +0530

    category page completed

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import style from "./Home.module.css";
 import TopNavbar from "../../Components/TopNavbar/TopNavbar";
+import Categorybar from "../../Components/Categorybar/Categorybar";
 function Home() {
   return (
     <div>
       <TopNavbar />
-      
+      <Categorybar/>
     </div>
   );
 }
